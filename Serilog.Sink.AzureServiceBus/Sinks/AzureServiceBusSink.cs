bb8f14d123ad9c993f8b70af69d34abacb847506
yongguan2001 <yong.guan@tc.gc.ca>
2021-02-18 15:54:34 -0500

    Added Serilog.Sink.AzureServiceBus library and test project.

---
+++ Serilog.Sink.AzureServiceBus/Sinks/AzureServiceBusSink.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Azure.ServiceBus;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Serilog.Core;
+using Serilog.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Serilog.Sinks.AzureServiceBus
+{
+    public class SerilLogEventModel
+    {
+        public LogEventLevel Level { set;  get; }
+        public string MessageTemplateName { set; get; }
+        public string MessageTemplate { set; get; }
+        public IReadOnlyDictionary<string, LogEventPropertyValue> Properties { set; get; }
+        public System.Exception Exception { set;  get; }
+    }
+
+    public class AzureServiceBusSink : ILogEventSink
+    {
+        readonly int _waitTimeoutInMilliseconds = Timeout.Infinite;
+        readonly IQueueClient _queueClient;
+        readonly IFormatProvider _formatProvider;
+
+        public AzureServiceBusSink(
+            IQueueClient queueClient,
+            IFormatProvider formatProvider)
+        {
+            _queueClient = queueClient;
+            _formatProvider = formatProvider;
+        }
+
+        /// <summary>
+        /// Emit the provided log event to the sink.
+        /// </summary>
+        /// <param name="logEvent">The log event to write.</param>
+        public void Emit(LogEvent logEvent)
+        {
+            SerilLogEventModel model = new SerilLogEventModel()
+            {
+                Level = logEvent.Level,
+                Properties = logEvent.Properties,
+                MessageTemplateName = logEvent.MessageTemplate.Text,
+                MessageTemplate= logEvent.RenderMessage(),
+                Exception = logEvent.Exception
+            };
+           
+            var jsonStr = JsonConvert.SerializeObject(model);
+            var message = new Message(Encoding.UTF8.GetBytes(jsonStr));
+            _queueClient.SendAsync(message).Wait(_waitTimeoutInMilliseconds);
+        }
+    }
+}
---
+++ Serilog.Sink.AzureServiceBus/Sinks/LoggerConfigurationAzureServiceBusExtensions.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Azure.ServiceBus;
+using Serilog.Configuration;
+using Serilog.Core;
+using Serilog.Events;
+using Serilog.Sinks.AzureServiceBus;
+using System;
+
+namespace Serilog
+{
+    public static class LoggerConfigurationAzureServiceBusExtensions
+    {
+        public static LoggerConfiguration AzureServiceBus(
+            this LoggerSinkConfiguration loggerConfiguration,
+            string serviceBusConnectionString,
+            string queueName,
+            IFormatProvider formatProvider = null,
+            LogEventLevel restrictedToMinimumLevel = LevelAlias.Minimum)
+        {
+            if (loggerConfiguration == null) throw new ArgumentNullException(nameof(loggerConfiguration));
+            if (string.IsNullOrWhiteSpace(serviceBusConnectionString)) throw new ArgumentNullException(nameof(serviceBusConnectionString));
+            if (string.IsNullOrWhiteSpace(queueName)) throw new ArgumentNullException(nameof(queueName));
+
+            try
+            {
+                QueueClient client = new QueueClient(serviceBusConnectionString, queueName);
+                return loggerConfiguration.Sink(new AzureServiceBusSink(client, formatProvider), restrictedToMinimumLevel);
+            }
+            catch (Exception ex)
+            {
+                Debugging.SelfLog.WriteLine($"Error configuring AzureServiceBus Sink : {ex.Message}");
+                ILogEventSink sink = new LoggerConfiguration().CreateLogger();
+                return loggerConfiguration.Sink(sink, restrictedToMinimumLevel);
+            }
+        }
+    }
+}
