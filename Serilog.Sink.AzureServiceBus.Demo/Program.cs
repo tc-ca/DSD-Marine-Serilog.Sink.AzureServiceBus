d35529bdb3acccf06f80da9c15f603c35dec41ac
yongguan2001 <yong.guan@tc.gc.ca>
2021-02-19 09:47:27 -0500

    Added demo project for Serilog.Sink.

---
+++ Serilog.Sink.AzureServiceBus.Demo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Serilog.Sink.AzureServiceBus.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var ConnectionString = "The real connection string for Azure Service Bus";
+            //var QueueName = "Name of the queue";
+            var ConnectionString = "Endpoint=sb://vessel-registry-service-bus.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=Cu2uP3XLV4GiH3bIsu7R7yztSNJpV74nE68g7l1Hknc=";
+            var QueueName = "vr-serilog-queue-test";
+
+            Log.Logger = new LoggerConfiguration()
+                .Enrich.FromLogContext()
+                .WriteTo.AzureServiceBus(ConnectionString, QueueName)
+                .CreateLogger();
+            Log.Information("Hello World!"); 
+
+        }
+    }
+}
